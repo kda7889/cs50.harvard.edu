87715514f21f7b2ea0a0592304acab6b098e1d3e
Danila <kolesnikov7889@yandex.ru>
2024-10-21 15:50:41 +0300

    Create hello.c

---
+++ C/hello.c
@@ -0,0 +1,16 @@
+// Программа на языке Си, которая выводит текст "hello, world" при запуске.
+// RU: Этот код просто выводит строку "hello, world" в консоль.
+// EN: This code simply prints the "hello, world" string to the console.
+// FR: Ce code imprime simplement la chaîne "hello, world" dans la console.
+// ES: Este código simplemente imprime la cadena "hello, world" en la consola.
+
+#include <stdio.h>
+
+int main() {
+    // RU: Функция printf выводит текст на консоль.
+    // EN: The printf function prints text to the console.
+    // FR: La fonction printf imprime le texte dans la console.
+    // ES: La función printf imprime texto en la consola.
+    printf("hello, world\n");
+    return 0;
+} 
